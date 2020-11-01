ee12909a750dc8cbe6299e14d552713077064125
Vantol Bennett (titan) <vvantol2000@icloud.com>
2020-11-01 16:21:59 -0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const fs = require('fs');
+const { exec } = require('child_process')
+
 
 function projectNext(options) {
-    fs.readFileSync('./project_gen.sh')
-
+    // fs.readFileSync('./project_gen.sh')
+    exec("git init", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    })
+    exec("mkdir scripts/ middlewares/ config/ controllers/ env/ utils/ models/ routes/ test/", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    })
 }
 module.exports = projectNext
